da3953a88b4ade35db05ea9bc17729a129cd152f
Muhammad Buhari <gmbdairy@gmail.com>
2021-07-25 21:28:41 +0100

    fixed route:email

--- resources/views/emails/posts/post_liked.blade.php
+++ resources/views/emails/posts/post_liked.blade.php
@@ -3,7 +3,7 @@
 
 {{ $liker->name }} liked one of your posts.
 
-@component('mail::button', ['url' => route('posts.show',$post)])
+@component('mail::button', ['url' => {{ route('posts.show',$post) }}])
 View Post
 @endcomponent
 
